0b8228339031519169df685a1a35e1f14bf935f3
Parva <parva.yadav@pivotroots.com>
2025-05-06 21:35:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import pandas as pd
+import pydeck as pdk
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Load city data
+data = pd.DataFrame({
+    'City': [...],
+    'Lat': [...],
+    'Lon': [...],
+    'Txn_Franchisee': [...],
+    'Frequency': [...],
+})
+
+# Pydeck map
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    data=data,
+    get_position='[Lon, Lat]',
+    get_radius=50000,
+    get_fill_color="[255 * (1 - Frequency/3300), 100, 200]",
+    pickable=True
 )
+
+view_state = pdk.ViewState(latitude=21, longitude=78, zoom=4)
+
+st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{City}\n{Frequency} Txns"}))
